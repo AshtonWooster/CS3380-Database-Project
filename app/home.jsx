09e411993d028a75b498f35c918a912adb9f44ec
Ashton Wooster <woosterashton48@gmail.com>
2024-12-08 15:32:27 -0600

    Init

---
+++ app/home.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App></App>);
